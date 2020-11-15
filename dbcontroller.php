15383d2ecedf33b38e3de6b35a5f6036fe7b572c
Hector Silva <51455691+silvahector@users.noreply.github.com>
2020-11-14 19:27:57 -0500

    Simple Cart with PHP
    Taken from phppot.com

---
+++ dbcontroller.php
@@ -0,0 +1,33 @@
+<?php
+class DBController {
+	private $host = "45.13.252.1";
+	private $user = "u645311621_hasc";
+	private $password = "#Ector9611171";
+	private $database = "u645311621_merkarco";
+	private $conn;
+	
+	function __construct() {
+		$this->conn = $this->connectDB();
+	}
+	
+	function connectDB() {
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $conn;
+	}
+	
+	function runQuery($query) {
+		$result = mysqli_query($this->conn,$query);
+		while($row=mysqli_fetch_assoc($result)) {
+			$resultset[] = $row;
+		}		
+		if(!empty($resultset))
+			return $resultset;
+	}
+	
+	function numRows($query) {
+		$result  = mysqli_query($this->conn,$query);
+		$rowcount = mysqli_num_rows($result);
+		return $rowcount;	
+	}
+}
+?>
